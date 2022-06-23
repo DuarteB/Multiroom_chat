6df2bbf93b8d4619ba7f88145a83f781da334ace
DuarteB <eduarte.dev@gmail.com>
2022-06-22 23:13:18 -0300

    server config and assets

---
+++ app/routes/chat.js
@@ -0,0 +1,9 @@
+module.exports = (app) => {
+    app.get('/chat', (req, res) => {
+        res.render('chat');
+    })
+
+    app.post('/chat', (req, res) => {
+        res.render('chat');
+    })
+}
